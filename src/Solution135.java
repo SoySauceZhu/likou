168cab9499ce98816a443a1196a6d92125bff305
Mingjie.Zhu <zhumingjie0627@outlook.com>
2025-01-28 22:46:59 +0000

    solution135 uncomplete

--- src/Solution135.java
+++ src/Solution135.java
@@ -1,112 +1,30 @@
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-
 public class Solution135 {
     public int candy(int[] ratings) {
-        int[] candies = new int[ratings.length];
+        for (int i = 0; i < ratings.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < ratings.length; j++) {
+                if (ratings[j] < ratings[minIndex]) {
+                    minIndex = j;
+                }
 
-        assignCandyRight(candies, ratings, 0);
-
-        int sum = 0;
-        for (int candy : candies) {
-            sum += candy;
+                int temp = ratings[j];
+                ratings[j] = ratings[minIndex];
+            }
         }
 
-        return sum;
-    }
+        int num = 0;
+        int lastCandy = 0;
 
-    private void assignCandyRight(int[] candyScheme, int[] ratings, int j) {
-        if (candyScheme.length == 1) {
-            candyScheme[0] = 1;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                num += lastCandy + 1;
+            }
+            if (ratings[i] < ratings[i - 1]) {
+
+            }
         }
 
-        if (j == ratings.length - 1) {
-            // rightmost
-            if (ratings[j] > ratings[j - 1]) {
-                assignCandyLeft(candyScheme, ratings, j - 1);
-                candyScheme[j] = candyScheme[j - 1] + 1;
-            } else {
-                candyScheme[j] = 1;
-            }
-        } else {
 
-//            if (j == 0) {
-//                // leftmost
-//                if (ratings[j] > ratings[j + 1]) {
-//                    assignCandy(candyScheme, ratings, j + 1);
-//                    candyScheme[j] = candyScheme[j + 1] + 1;
-//                } else {
-//                    candyScheme[j] = 1;
-//                }
-//            } else {
-
-
-            assignCandyRight(candyScheme, ratings, j + 1);
-
-            if (ratings[j] > max(ratings[j + 1], ratings[j - 1])) {
-                candyScheme[j] = max(candyScheme[j + 1], candyScheme[j - 1]) + 1;
-            }
-            if (ratings[j] < min(ratings[j + 1], ratings[j - 1])) {
-                candyScheme[j] = 1;
-            } else {
-                candyScheme[j] = min(candyScheme[j + 1], candyScheme[j - 1]) + 1;
-            }
-
-            assignCandyLeft(candyScheme, ratings, j - 1);
-
-        }
-    }
-
-    private void assignCandyLeft(int[] candyScheme, int[] ratings, int j) {
-        if (candyScheme.length == 1) {
-            candyScheme[0] = 1;
-        }
-
-        if (j == 0) {
-            // rightmost
-            if (ratings[j] > ratings[j + 1]) {
-                assignCandyLeft(candyScheme, ratings, j - 1);
-                candyScheme[j] = candyScheme[j + 1] + 1;
-            } else {
-                candyScheme[j] = 1;
-            }
-        } else {
-
-//            if (j == 0) {
-//                // leftmost
-//                if (ratings[j] > ratings[j + 1]) {
-//                    assignCandy(candyScheme, ratings, j + 1);
-//                    candyScheme[j] = candyScheme[j + 1] + 1;
-//                } else {
-//                    candyScheme[j] = 1;
-//                }
-//            } else {
-
-
-            assignCandyLeft(candyScheme, ratings, j - 1);
-
-            if (ratings[j] > max(ratings[j + 1], ratings[j - 1])) {
-                candyScheme[j] = max(candyScheme[j + 1], candyScheme[j - 1]) + 1;
-            }
-            if (ratings[j] < min(ratings[j + 1], ratings[j - 1])) {
-                candyScheme[j] = 1;
-            } else {
-                candyScheme[j] = min(candyScheme[j + 1], candyScheme[j - 1]) + 1;
-            }
-
-            assignCandyRight(candyScheme, ratings, j + 1);
-
-        }
-
-    }
-
-
-    public static void main(String[] args) {
-        int[] a = new int[]{5, 4, 3, 2, 1};
-        Solution135 s = new Solution135();
-        int b = s.candy(a);
-        System.out.println(b);
+        return num;
     }
 }
-
-
