605c0b6908f6942dcd8072742c5595c597cb4bf2
Mingjie.Zhu <zhumingjie0627@outlook.com>
2025-01-30 23:10:29 +0000

    add Solution121

---
+++ src/Solution121.java
@@ -0,0 +1,23 @@
+public class Solution121 {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        Solution121 solution121 = new Solution121();
+        System.out.println(solution121.maxProfit(prices));
+    }
+}
